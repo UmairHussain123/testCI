039a7f91cddc31a426160172631135d570080e95
ZubairShaikh696 <67466633+ZubairShaikh696@users.noreply.github.com>
2022-09-06 11:16:57 +0500

    updated cs2

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4
+{
+    public class Class1
+    {
+
+        public string name1 = "";
+        public string age = "";
+        public string educ = "";
+    
+        //public static string Xyz(string abc,string abc2)
+        //{
+        //    return abc + abc2;
+        //}
+
+}
+}
